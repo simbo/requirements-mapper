0ffedd0df3ac1c5c4cacccb2ad22a01b91557573
Simon Lepel <simbo@simbo.de>
2015-07-29 10:49:48 +0200

    add tests and fixtures

---
+++ test/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var assert = require('assert');
+
+var pgkJson = require('../package.json'),
+    pkgName = pgkJson.name,
+    pkg = require('..');
+
+var expectations = {
+        basic: {
+            bar: true,
+            baz: {
+                bada: true,
+                bing: true,
+                ping: {
+                    pong: true
+                }
+            },
+            foo: true
+        },
+        overwrite: {
+            foo: {
+                bar: true
+            }
+        }
+    };
+
+describe(pkgName, function() {
+
+    it('should return recursively required contents of a directory as object', function() {
+        assert.deepEqual(pkg('fixtures/basic'), expectations.basic);
+    });
+
+    it('should overwrite the required contents of a file if there is a directory with the same name on the same level', function() {
+        assert.deepEqual(pkg('fixtures/overwrite'), expectations.overwrite);
+    });
+
+});
