e1ea47a0288a79e2b874f38dd1898aedb0fc671f
Simon Lepel <simbo@simbo.de>
2015-07-30 03:13:35 +0200

    add gulp tasks for watching and testing

---
+++ Gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var gulp = require('gulp'),
+    mocha = require('gulp-mocha');
+
+gulp.task('watch', ['test'], function () {
+    gulp.watch([
+        './lib/index.js',
+        './test/index.js'
+    ], ['test']);
+});
+
+gulp.task('test', function () {
+    return gulp
+        .src('./test/index.js', {read: false})
+        .pipe(mocha());
+});
+
+gulp.task('default', ['test']);
