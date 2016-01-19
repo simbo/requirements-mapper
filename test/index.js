a805adf45aff7924db939d4ff0d0867c90c2ffa1
Simon Lepel <simbo@simbo.de>
2016-01-19 23:09:58 +0100

    switch from jshint+jscs to eslint; reformat sources according to new linting rules

--- test/index.js
+++ test/index.js
@@ -3,81 +3,83 @@
 var assert = require('assert'),
     path = require('path');
 
-var pkgJson = require('../package.json'),
-    pkgName = pkgJson.name,
-    Pkg = require('..');
+var Pkg = require('..');
+
+var pkgJson = require('../package.json');
+
+var pkgName = pkgJson.name;
 
 var expectations = {
+  basic: {
+    bar: true,
+    baz: {
+      bada: true,
+      bing: true,
+      ping: {
+        pong: true
+      }
+    },
+    foo: true
+  },
+  overwrite: {
+    foo: {
+      bar: true
+    }
+  },
+  simple: {
+    foo: true
+  },
+  fallback: {
+    test: {
+      fixtures: {
         basic: {
-            bar: true,
-            baz: {
-                bada: true,
-                bing: true,
-                ping: {
-                    pong: true
-                }
-            },
-            foo: true
-        },
-        overwrite: {
-            foo: {
-                bar: true
-            }
-        },
-        simple: {
-            foo: true
-        },
-        fallback: {
-            test: {
-                fixtures: {
-                    basic: {
-                        foo: true
-                    }
-                }
-            }
+          foo: true
         }
-    };
+      }
+    }
+  }
+};
 
 describe(pkgName, function() {
 
-    it('should return recursively required contents of a directory as object', function() {
-        assert.deepEqual(new Pkg('fixtures/basic').map(), expectations.basic);
-    });
+  it('should return recursively required contents of a directory as object', function() {
+    assert.deepEqual(new Pkg('fixtures/basic').map(), expectations.basic);
+  });
 
-    it('should accept absolute paths', function() {
-        assert.deepEqual(new Pkg(path.join(process.cwd(), 'test/fixtures/basic')).map(), expectations.basic);
-    });
+  it('should accept absolute paths', function() {
+    assert.deepEqual(new Pkg(path.join(process.cwd(), 'test/fixtures/basic')).map(), expectations.basic);
+  });
 
-    it('should accept a cwd for globbing and require', function() {
-        var p = new Pkg();
-        p.cwd = path.join(process.cwd(), 'test/fixtures/basic');
-        assert.deepEqual(p.map(), expectations.basic);
-    });
+  it('should accept a cwd for globbing and require', function() {
+    var pkg = new Pkg();
+    pkg.cwd = path.join(process.cwd(), 'test/fixtures/basic');
+    assert.deepEqual(pkg.map(), expectations.basic);
+  });
 
-    it('should overwrite existing properties depending on alphabetic order of found files and directories', function() {
-        assert.deepEqual(new Pkg('fixtures/overwrite').map(), expectations.overwrite);
-    });
+  it('should overwrite existing properties depending on alphabetic order of found files and directories', function() {
+    assert.deepEqual(new Pkg('fixtures/overwrite').map(), expectations.overwrite);
+  });
 
-    it('should accept custom glob patterns', function() {
-        var p = new Pkg('fixtures/basic');
-        p.globPattern = '**/*.js';
-        assert.deepEqual(p.map(), expectations.simple);
-    });
+  it('should accept custom glob patterns', function() {
+    var pkg = new Pkg('fixtures/basic');
+    pkg.globPattern = '**/*.js';
+    assert.deepEqual(pkg.map(), expectations.simple);
+  });
 
-    it('should accept custom glob options', function() {
-        var p = new Pkg('fixtures/overwrite');
-        p.globOptions = {ignore: 'foo/**/*.js?(on)'};
-        assert.deepEqual(p.map(), expectations.simple);
-    });
+  it('should accept custom glob options', function() {
+    var pkg = new Pkg('fixtures/overwrite');
+    pkg.globOptions = {ignore: 'foo/**/*.js?(on)'};
+    assert.deepEqual(pkg.map(), expectations.simple);
+  });
 
-    it('should fallback to process.cwd() when no directory is set', function() {
-        var p = new Pkg();
-        p.globPattern = 'test/fixtures/basic/*.js';
-        assert.deepEqual(p.map(), expectations.fallback);
-    });
+  it('should fallback to process.cwd() when no directory is set', function() {
+    var pkg = new Pkg();
+    pkg.globPattern = 'test/fixtures/basic/*.js';
+    assert.deepEqual(pkg.map(), expectations.fallback);
+  });
 
-    it('should be possible to use relative paths when using it as submodule via require.', function() {
-        assert.deepEqual(require('./fixtures/module'), expectations.simple);
-    });
+  it('should be possible to use relative paths when using it as submodule via require.', function() {
+    assert.deepEqual(require('./fixtures/module'), expectations.simple);
+  });
 
 });
